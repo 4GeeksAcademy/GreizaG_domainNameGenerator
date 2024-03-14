44f64c9b02780d07a8fa49bbad6b835bd732f011
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-03-14 01:49:31 +0000

    [Cambio nombre variables]

--- domainNameGenerator.js
+++ domainNameGenerator.js
@@ -2,10 +2,10 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-for (let pro in pronoun){
-    for (let ad in adj){
-        for (let nou in noun){
-            console.log(pronoun[pro]+adj[ad]+noun[nou]+".com")
-        }
+for (let pro in pronoun) {
+  for (let ad in adj) {
+    for (let nou in noun) {
+      console.log(pronoun[pro] + adj[ad] + noun[nou] + ".com");
     }
+  }
 }
